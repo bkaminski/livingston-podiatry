55e02fdb0f81db7d25acd50bcd97f1d51845ee5c
Ben Kaminski <ben@benkaminski.com>
2020-11-03 19:58:48 -0500

    centering at mobile of title area

--- assets/js/partials/_responsive.js
+++ assets/js/partials/_responsive.js
@@ -10,12 +10,16 @@
 			$('.nav--logo').width(260);
 			$('.footer--right').find('p').removeClass('text-right');
 			$('.foot--social').removeClass('text-center');
+			$('.hero--1').addClass('text-center').removeClass('float-left');
+			$('.hero--2').addClass('text-center').removeClass('float-right');
 		};
 		if ($window.width() >= 976) {
 			$('.navbar-collapse').addClass('.justify-content-md-center');
 			$('.nav--logo').width(350);
 			$('.footer--right').find('p').addClass('text-right');
 			$('.foot--social').addClass('text-center');
+			$('.hero--1').removeClass('text-center').addClass('float-left');
+			$('.hero--2').removeClass('text-center').addClass('float-right');
 		}
 	}
 	
